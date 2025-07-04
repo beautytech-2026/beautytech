74ecc00e543df0bd8ea9cbe1286c921622322209
sasa snfs <sasasnfs@sasas-MacBook-Air.local>
2025-07-04 17:37:08 +0100

    fix: clear cache with service worker

--- sw.js
+++ sw.js
@@ -1,15 +1,20 @@
-self.addEventListener('install', function(event) {
+self.addEventListener('install', event => {
+    // Force this SW to become active immediately
     self.skipWaiting();
   });
   
-  self.addEventListener('activate', function(event) {
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(cacheName) {
-          return caches.delete(cacheName);
-        })
-      );
-    });
+  self.addEventListener('activate', event => {
+    event.waitUntil(
+      caches.keys().then(cacheNames => {
+        return Promise.all(
+          cacheNames.map(cache => {
+            return caches.delete(cache); // Delete all caches
+          })
+        );
+      }).then(() => {
+        return self.clients.claim(); // Take control of all pages immediately
+      })
+    );
   });
-  
+
   
